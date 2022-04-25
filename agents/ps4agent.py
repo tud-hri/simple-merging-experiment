e37ffc4c1eea3133931e081b7a9a90898964050f
O. Siebinga <o.siebinga@tudelft.nl>
2022-04-25 13:47:11 +0200

    added license

--- agents/ps4agent.py
+++ agents/ps4agent.py
@@ -1,3 +1,21 @@
+"""
+Copyright 2022, Olger Siebinga (o.siebinga@tudelft.nl)
+
+This file is part of simple-merging-experiment.
+
+simple-merging-experiment is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+simple-merging-experiment is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with simple-merging-experiment.  If not, see <https://www.gnu.org/licenses/>.
+"""
 import enum
 
 import inputs
